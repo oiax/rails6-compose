47ce343f6e39e185b70cee0017e82f7656e5d087
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-02-18 02:12:29 +0900

    Add 'msys' to setup.sh (for windows 10 pro)

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ case "$OSTYPE" in
   darwin*)
     docker-compose build web
     ;;
+  msys*)
+    docker-compose build web
+    ;;
   linux*)
     docker-compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) web
     ;;
